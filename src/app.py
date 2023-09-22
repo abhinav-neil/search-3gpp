e1dfe5861688c1e092fb4faa4d30877adcfe6654
abhinav-neil <abhinav.neil@gmail.com>
2023-09-22 11:44:27 +0000

    resolve file links on UI

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
@@ -12,11 +12,25 @@ def index():
         query = request.form['query']
         response = es.search(index='3gpp_docs', body={"query": {"match": {"content": query}}})
         for hit in response["hits"]["hits"]:
+            filename = hit["_source"]["filename"]
+            download_url = f"/files/download/{filename}"
+            view_url = f"/files/view/{filename}"
             results.append({
-                "filename": hit["_source"]["filename"],
-                "url": hit["_source"]["url"]
+                "filename": filename,
+                "download_url": download_url,
+                "view_url": view_url
             })
     return render_template('index.html', results=results, query=query)
+
+@app.route('/files/<action>/<path:filename>', methods=['GET'])
+def serve_file(action, filename):
+    if action == "download":
+        return send_from_directory('/home/neil/docscan-philips/data/raw_docs/', filename, as_attachment=True)
+    elif action == "view":
+        return send_from_directory('/home/neil/docscan-philips/data/raw_docs/', filename, as_attachment=False)
+    else:
+        return "Invalid action", 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
